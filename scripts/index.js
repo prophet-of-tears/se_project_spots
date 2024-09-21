a9bd7e4c988b21f2f9951f32c793b4422fbef55d
Jeremiah P <speceod@gmail.com>
2024-09-21 05:10:41 -0700

    new script

--- scripts/index.js
+++ scripts/index.js
@@ -12,62 +12,99 @@ const initialCards = [
     {name: "Tunnel with morning light", 
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg"},
 ];
-console.log(initialCards);
 
-const profileEditButton = document.querySelector(".profile__edit-btn");
-const modal = document.querySelector("#edit-profile-modal");
+
+const profileEditButton = document.querySelector("#edit-modal");
+const modal = document.querySelector("#edit-profile-modal"); // edit modal div
+const newModal = document.querySelector("#new-post-modal"); // image modal div
+const modalClose = document.querySelector("#close-Button"); // was attached to exit button for the add button
 
 const profileName = document.querySelector("#name-input");
 const profileDescription = document.querySelector("#description-input");
-const profileForm = document.querySelector(".modal__form");
+const profileForm = document.querySelector(".modal__form"); // edit form
 const nameOnPage = document.querySelector("#profile-name");
 const descriptionOnPage = document.querySelector("#profile-description");
+const modalExitButton = modal.querySelector("#exit-button"); // exit button for both modals
 
-function openModal(){
-  profileName.value = nameOnPage.textContent;
-  profileDescription.value = descriptionOnPage.textContent;
+const profileAddButton = document.querySelector(".profile__add-btn");
+const modalImageForm = newModal.querySelector(".modal__form"); // image Form
+const imageLink = newModal.querySelector("#link-input");
+const caption = newModal.querySelector("#caption-input");
+const imageSaveButton = newModal.querySelector("#save-image");
+const newModalCloseButton = newModal.querySelector(".modal__exit-btn");
+
+function openModal(modal) {
   modal.classList.add("modal_opened");
 }
 
-const modalExitButton = document.querySelector("#exit-button");
-
-function closeModal(){
+function closeModal(modal) {
     modal.classList.remove("modal_opened");
 }
+
+
+newModalCloseButton.addEventListener("click", () =>{
+  closeModal(newModal);
+});
+
+
+profileAddButton.addEventListener("click", () => {
+  openModal(newModal);
+});
+
+
+modalExitButton.addEventListener("click", () => {
+  closeModal(modal);
+});
+
+
+function handleImageFormSubmit(evt) {
+  evt.preventDefault();
+  const inputValues = {name: caption.value, link: imageLink.src};
+  const cardsElement = getCardElement(inputValues);
+  cardsList.prepend(cardsElement);
+ closeModal(newModal);
+};
+
+modalImageForm.addEventListener("submit", handleImageFormSubmit);
+
+profileEditButton.addEventListener("click", () => {
+  profileName.value = nameOnPage.textContent;
+  profileDescription.value = descriptionOnPage.textContent;
+  openModal(modal);
+});
+
 
 
 function handleProfileFormSubmit(evt) {
   evt.preventDefault();
   nameOnPage.textContent = profileName.value;
   descriptionOnPage.textContent = profileDescription.value;
-  closeModal();
+  closeModal(modal);
 }
 
-
-modalExitButton.addEventListener("click", closeModal);
-profileEditButton.addEventListener( "click", openModal);
 profileForm.addEventListener("submit", handleProfileFormSubmit);
 
-const templateElement = document.querySelector("#card-template");
+
 const cardsList = document.querySelector(".cards__list");
 
-
-
 function getCardElement(data){
-  console.log(data);
+  const templateElement = document.querySelector("#card-template").content;
   const cardElement = templateElement
-  .content.querySelector(".card")
+  .querySelector(".card")
   .cloneNode(true);
-const cardName = cardElement.querySelector(".card__title");
-const cardsImage = cardElement.querySelector(".card__image");
+const cardName = cardElement.querySelector("#card_title");
+const cardsImage = cardElement.querySelector("#card_image_content");
 cardName.textContent = data.name;
 cardsImage.alt = data.name;
 cardsImage.src = data.link;
 
 return cardElement;
-}
+};
 
-for (let i = 0; i < initialCards.length; i++){
-   const cardsElement = getCardElement(initialCards[i]);
-   cardsList.prepend(cardsElement);
-}
+
+
+initialCards.forEach((card) => {
+  const cardsElement = getCardElement(card);
+  cardsList.append(cardsElement);
+  return card;
+});
